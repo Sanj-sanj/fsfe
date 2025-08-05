f868819a1b520e2d4f91497b6460372d9f8cfe74
Sanjeet Sidhu <Sanjeetu3@gmail.com>
2025-08-04 18:59:39 -0700

    Created websocket server

---
+++ socket.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const server = require('http').createServer()
+const app = express()
+
+app.get('/', function (req,res) {
+   res.sendFile('index.html', {root: __dirname}); 
+})
+
+server.on('request', app)
+server.listen(3000, function() { console.log('Server started on PORT 3000') })
+
+/**
+ * BEGIN WEBSOCKETS
+ * */
+
+const WebSocketServer = require('ws').Server;  
+
+const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size
+  console.log('Clients connected: (node\) ', numClients)
+
+  wss.broadcast(`Current visitors: ${numClients}`)
+
+  if(ws.readyState === ws.OPEN) {
+    ws.send('Welcome to my server')
+  }
+  
+  ws.on('close', function close() {
+    wss.broadcast(`Current visitors = ${numClients}`)
+    console.log('A client has disconnected node')
+  })
+})
+  
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data)
+  })
+}
